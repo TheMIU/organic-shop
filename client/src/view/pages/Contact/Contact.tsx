c50221c2e3a0196a06f08f4f1c5b08ef5235c2d4
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-01-05 12:10:20 +0530

    contact form submit

--- client/src/view/pages/Contact/Contact.tsx
+++ client/src/view/pages/Contact/Contact.tsx
@@ -1,6 +1,33 @@
+import axios from 'axios';
 import React, { Component } from 'react'
 
-export default class Contact extends Component {
+interface ContactProps {
+  data: any;
+}
+
+interface ContactState {
+  name: string;
+  email: string;
+  message: string;
+}
+
+export default class Contact extends Component<ContactProps, ContactState> {
+
+  private api: any;
+
+  constructor(props: any) {
+    super(props);
+    this.api = axios.create({ baseURL: `http://localhost:4000` });
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    }
+
+    this.handleMessageInputOnChange = this.handleMessageInputOnChange.bind(this);
+  }
+
+
   render() {
     return (
       <div className=" h-screen text-center bg-[#0e7575]">
@@ -16,6 +43,8 @@ export default class Contact extends Component {
                 type="text"
                 id="name"
                 name="name"
+                value={this.state.name}
+                onChange={this.handleMessageInputOnChange}
                 className="w-full border-2 border-blue-500 rounded-md py-2 px-3 focus:outline-none focus:border-blue-900"
                 placeholder="John Doe"
               />
@@ -29,6 +58,8 @@ export default class Contact extends Component {
                 type="email"
                 id="email"
                 name="email"
+                value={this.state.email}
+                onChange={this.handleMessageInputOnChange}
                 className="w-full border-2 border-blue-500 rounded-md py-2 px-3 focus:outline-none focus:border-blue-900"
                 placeholder="john.doe@example.com"
               />
@@ -41,15 +72,45 @@ export default class Contact extends Component {
               <textarea
                 id="message"
                 name="message"
+                value={this.state.message}
+                onChange={this.handleMessageInputOnChange}
                 className="w-full border-2 border-blue-500 rounded-md py-2 px-3 focus:outline-none focus:border-blue-900"
                 placeholder="Write your message here..."
               ></textarea>
             </div>
 
-            <button type="submit" className="add-to-cart-btn"> Send Message </button>
+            <button type="button" className="add-to-cart-btn" onClick={this.onSendBtnClick}> Send Message </button>
           </form>
         </div>
       </div>
     )
+  }
+
+  handleMessageInputOnChange(event: { target: { value: any; name: any; } }) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    // @ts-ignore
+    this.setState({
+      [name]: value
+    });
+  }
+
+  private onSendBtnClick = () => {
+    try {
+      this.api.post('/contact/submit', {
+        name: this.state.name,
+        email: this.state.email,
+        message: this.state.message,
+      }).then((res: { data: any }) => {
+        const jsonData = res.data;
+        alert(jsonData);
+      }).catch((error: any) => {
+        console.error("axios error : ", error);
+      })
+    } catch (error) {
+      console.error("Error submitting data ", error);
+    }
   }
 }
